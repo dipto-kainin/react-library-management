ba80de909b6026336df9027982806060e8be1fc3
dipto-kainin <kaininhop@gmail.com>
2024-08-14 02:48:01 +0530

    build

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const cors=require("cors");
 const userRoutes = require("./routes/userRoutes");
 const bookRoutes = require("./routes/bookRoutes");
+const path = require('path')
 dotenv.config();
 connectDB();
 const app = express();
@@ -11,6 +12,23 @@ app.use(express.json());
 app.use(cors());
 app.use('/api/user',userRoutes);
 app.use('/api/book',bookRoutes);
+//------------------------deployment----------------------------------
+
+const __dirname1 = path.resolve();
+if(process.env.NODE_ENV){
+    app.use(express.static(path.join(__dirname1,'/frontend/build')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname1,'/frontend/build/index.html'));
+    })
+}else{
+    app.get('/',(req,res)=>{
+        res.send('Server is running...')
+    })
+}
+
+
+//------------------------deployment----------------------------------
+
 const port = process.env.PORT || 4000;
 
 app.listen(port,console.log(`server started on port ${port}`));
